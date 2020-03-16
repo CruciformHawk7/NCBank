abc645bebcbca6126ae1ffc5ed4e92ebe65a8f0c
Nikhil Hari <nikhil_hari@live.com>
2020-03-17 03:17:08 +0530

    Add DB Connection, Fix Model

--- Models/BankCustomer.cs
+++ Models/BankCustomer.cs
@@ -3,14 +3,41 @@ using System.ComponentModel.DataAnnotations;
 
 namespace NCBank.Models {
     public class BankCustomer {
-        [Required(ErrorMessage = "First Name is required")]
+        private string _Password;
+        [Required(ErrorMessage="Enter your First Name")]
         public string FirstName { get; set; }
-        [Required(ErrorMessage = "Last Name is required")]
+        [Required(ErrorMessage="Enter your Last Name")]
         public string LastName { get; set; }
-        [Required(ErrorMessage = "Title is required")]
-        public string Title { get; set; }
-        [Required(ErrorMessage = "Account Creation Time Error")]
-        public DateTime AccountCreation { get; set; }
+        public string Email {get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public string MaritalStatus { get; set; }
+        public string Password { 
+            get { return _Password; } 
+            set {
+                _Password = Hash(value);
+            } 
+        } 
         
+        public string HouseName { get; set; }
+        public string HouseNumber { get; set; }
+        public string FirstAddress { get; set; }
+        public string SecondAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Phone { get; set; }
+        
+        public string JobTitle { get; set; }
+        public string OrgName { get; set; }
+        public string OrgCity { get; set; }
+        public string OrgPhone { get; set; }
+        
+        public string Aadhar { get; set; }
+        public string Pan { get; set; }
+
+
+        private string Hash(string password) {
+            return password;
+        }
     }
 }
--- Pages/Register.cshtml.cs
+++ Pages/Register.cshtml.cs
@@ -1,37 +1,43 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using MongoDB.Bson;
 using MongoDB.Driver;
+using NCBank.Models;
 
 namespace NCBank.Pages {
     public class Register : PageModel {
-        
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public int Age { get; set; }
-        public string Gender { get; set; }
-        public string MaritalStatus { get; set; }
-        
-        public string HouseName { get; set; }
-        public string HouseNumber { get; set; }
-        public string FirstAddress { get; set; }
-        public string SecondAddress { get; set; }
-        public string City { get; set; }
-        public string State { get; set; }
-        public string Phone { get; set; }
-        
-        public string JobTitle { get; set; }
-        public string OrgName { get; set; }
-        public string OrgCity { get; set; }
-        public string OrgPhone { get; set; }
-        
-        public string Aadhar { get; set; }
-        public string Pan { get; set; }
+        [BindProperty]
+        public BankCustomer customer {get; set; }
         
         public void OnPost() {
             var db = new MongoClient().GetDatabase("NCBank");
-            var customer = db.GetCollection<BsonDocument>("Customer");
-            BsonDocument doc = new BSonDocument();
-            
+            var cust = db.GetCollection<BsonDocument>("Customer");
+            BsonDocument doc = new BsonDocument();
+            doc.Add(new BsonElement("FirstName", customer.FirstName));
+            doc.Add(new BsonElement("LastName", customer.LastName));
+            doc.Add(new BsonElement("Email", customer.Email));
+            doc.Add(new BsonElement("Age", customer.Age));
+            doc.Add(new BsonElement("Gender", customer.Gender));
+            doc.Add(new BsonElement("MaritalStatus", customer.MaritalStatus));
+            doc.Add(new BsonElement("PasswordHash", customer.Password));
+
+            doc.Add(new BsonElement("HouseName", customer.HouseName));
+            doc.Add(new BsonElement("HouseNumber", customer.HouseNumber));
+            doc.Add(new BsonElement("FirstAddress", customer.FirstAddress));
+            doc.Add(new BsonElement("SecondAddress", customer.SecondAddress));
+            doc.Add(new BsonElement("City", customer.City));
+            doc.Add(new BsonElement("State", customer.State));
+            doc.Add(new BsonElement("Phone", customer.Phone));
+
+            doc.Add(new BsonElement("JobTitle", customer.JobTitle));
+            doc.Add(new BsonElement("OrgName", customer.OrgName));
+            doc.Add(new BsonElement("OrgCity", customer.OrgCity));
+            doc.Add(new BsonElement("OrgPhone", customer.OrgPhone));
+
+            doc.Add(new BsonElement("Aadhar", customer.Aadhar));
+            doc.Add(new BsonElement("Pan", customer.Pan));
+
+            cust.InsertOne(doc);
             
             Response.Redirect("/Index");
         }
