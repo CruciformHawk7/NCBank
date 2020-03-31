55700d99bafbe2355df9a24379d6f64a60aa09ae
Nikhil Hari <nikhil_hari@live.com>
2020-04-01 02:50:08 +0530

    BankCustomer model: Refractor.

--- Models/BankCustomer.cs
+++ Models/BankCustomer.cs
@@ -5,6 +5,51 @@ using System;
 using MongoDB.Bson;
 
 namespace NCBank.Models {
+
+    internal static class Hashing {
+        internal static string Hash(string password) {
+            string op = "";
+            using (MD5 md5Hash = MD5.Create()){
+                op = GetMd5Hash(md5Hash, password);
+            }
+            return op;
+        }
+
+        static private string GetMd5Hash(MD5 md5Hash, string input) {
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+            StringBuilder sBuilder = new StringBuilder();
+            for (int i = 0; i < data.Length; i++)
+                sBuilder.Append(data[i].ToString("x2"));
+            return sBuilder.ToString();
+        }
+
+        static internal bool VerifyMd5Hash(MD5 md5Hash, string input, string hash) {
+            string hashOfInput = GetMd5Hash(md5Hash, input);
+            StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+            if (0 == comparer.Compare(hashOfInput, hash))
+                return true;
+            else
+                return false;
+        }
+    }
+
+    public class LoggedInCustomer {
+        private string _Password;
+        public string Email {get; set; }
+        public string Password {
+            get { return _Password; }
+            set { _Password = Hashing.Hash(value); }
+        }
+
+        public bool verifyPassword(string password) {
+            bool op = false;
+            using (MD5 md5hash = MD5.Create()) {
+                op = Hashing.VerifyMd5Hash(md5hash, this.Password, password);
+            }
+            return op;
+        }
+
+    }
     public class BankCustomer {
         private string _Password;
         [Required(ErrorMessage="Enter your First Name")]
@@ -17,7 +62,7 @@ namespace NCBank.Models {
         public string MaritalStatus { get; set; }
         public string Password { 
             get { return _Password; } 
-            set { _Password = Hash(value); } 
+            set { _Password = Hashing.Hash(value); } 
         } 
         
         public string HouseName { get; set; }
@@ -35,40 +80,6 @@ namespace NCBank.Models {
         
         public string Aadhar { get; set; }
         public string Pan { get; set; }
-
-
-        private string Hash(string password) {
-            string op = "";
-            using (MD5 md5Hash = MD5.Create()){
-                op = GetMd5Hash(md5Hash, password);
-            }
-            return op;
-        }
-
-        public bool verifyPassword(string password) {
-            bool op = false;
-            using (MD5 md5hash = MD5.Create()) {
-                op = VerifyMd5Hash(md5hash, this.Password, password);
-            }
-            return op;
-        }
-
-        static private string GetMd5Hash(MD5 md5Hash, string input) {
-            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
-            StringBuilder sBuilder = new StringBuilder();
-            for (int i = 0; i < data.Length; i++)
-                sBuilder.Append(data[i].ToString("x2"));
-            return sBuilder.ToString();
-        }
-
-        static private bool VerifyMd5Hash(MD5 md5Hash, string input, string hash) {
-            string hashOfInput = GetMd5Hash(md5Hash, input);
-            StringComparer comparer = StringComparer.OrdinalIgnoreCase;
-            if (0 == comparer.Compare(hashOfInput, hash))
-                return true;
-            else
-                return false;
-        }
 
         public BsonDocument GetBson() {
             BsonDocument doc = new BsonDocument();
