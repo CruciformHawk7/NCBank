f823dd0103c88ce735bde40331dfbe4dbb5c3f53
Nikhil Hari <nikhil_hari@live.com>
2020-04-02 01:01:36 +0530

    Follow MongoDB Naming guidelines

--- Models/BankCustomer.cs
+++ Models/BankCustomer.cs
@@ -3,6 +3,7 @@ using System.Security.Cryptography;
 using System.Text;
 using System;
 using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace NCBank.Models {
 
@@ -35,6 +36,9 @@ namespace NCBank.Models {
 
     public class LoggedInCustomer {
         private string _Password;
+
+        [BsonId]
+        public ObjectId Id {get; set; }
         public string Email {get; set; }
         public string Password {
             get { return _Password; }
@@ -52,6 +56,8 @@ namespace NCBank.Models {
     }
     public class BankCustomer {
         private string _Password;
+        [BsonId]
+        public ObjectId Id {get; set; }
         [Required(ErrorMessage="Enter your First Name")]
         public string FirstName { get; set; }
         [Required(ErrorMessage="Enter your Last Name")]
@@ -83,29 +89,29 @@ namespace NCBank.Models {
 
         public BsonDocument GetBson() {
             BsonDocument doc = new BsonDocument();
-            doc.Add(new BsonElement("FirstName", FirstName));
-            doc.Add(new BsonElement("LastName", LastName));
-            doc.Add(new BsonElement("Email", Email));
-            doc.Add(new BsonElement("Age", Age));
-            doc.Add(new BsonElement("Gender", Gender));
-            doc.Add(new BsonElement("MaritalStatus", MaritalStatus));
-            doc.Add(new BsonElement("PasswordHash", Password));
+            doc.Add(new BsonElement("firstName", FirstName));
+            doc.Add(new BsonElement("lastName", LastName));
+            doc.Add(new BsonElement("email", Email));
+            doc.Add(new BsonElement("age", Age));
+            doc.Add(new BsonElement("gender", Gender));
+            doc.Add(new BsonElement("maritalStatus", MaritalStatus));
+            doc.Add(new BsonElement("passwordHash", Password));
 
-            doc.Add(new BsonElement("HouseName", HouseName));
-            doc.Add(new BsonElement("HouseNumber", HouseNumber));
-            doc.Add(new BsonElement("FirstAddress", FirstAddress));
-            doc.Add(new BsonElement("SecondAddress", SecondAddress));
-            doc.Add(new BsonElement("City", City));
-            doc.Add(new BsonElement("State", State));
-            doc.Add(new BsonElement("Phone", Phone));
+            doc.Add(new BsonElement("houseName", HouseName));
+            doc.Add(new BsonElement("houseNumber", HouseNumber));
+            doc.Add(new BsonElement("firstAddress", FirstAddress));
+            doc.Add(new BsonElement("secondAddress", SecondAddress));
+            doc.Add(new BsonElement("city", City));
+            doc.Add(new BsonElement("state", State));
+            doc.Add(new BsonElement("phone", Phone));
 
-            doc.Add(new BsonElement("JobTitle", JobTitle));
-            doc.Add(new BsonElement("OrgName", OrgName));
-            doc.Add(new BsonElement("OrgCity", OrgCity));
-            doc.Add(new BsonElement("OrgPhone", OrgPhone));
+            doc.Add(new BsonElement("jobTitle", JobTitle));
+            doc.Add(new BsonElement("orgName", OrgName));
+            doc.Add(new BsonElement("orgCity", OrgCity));
+            doc.Add(new BsonElement("orgPhone", OrgPhone));
 
-            doc.Add(new BsonElement("Aadhar", Aadhar));
-            doc.Add(new BsonElement("Pan", Pan));
+            doc.Add(new BsonElement("aadhar", Aadhar));
+            doc.Add(new BsonElement("pan", Pan));
 
             return doc;
         }
--- Models/DatabaseActions.cs
+++ Models/DatabaseActions.cs
@@ -3,8 +3,8 @@ using MongoDB.Driver;
 
 namespace NCBank.Models {
     static class DBInterface {
-        static IMongoDatabase db = new MongoClient().GetDatabase("NCBank");
-        static IMongoCollection<BsonDocument> cust = db.GetCollection<BsonDocument>("Customer");
+        static IMongoDatabase db = new MongoClient().GetDatabase("bankNC");
+        static internal IMongoCollection<BsonDocument> cust = db.GetCollection<BsonDocument>("customers");
 
         static public void InsertCustomer(BsonDocument doc) {
             cust.InsertOne(doc);
