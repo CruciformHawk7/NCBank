08e9b107ccf15013fa59d2705b86f9a14703cdd4
Nikhil Hari <nikhil_hari@live.com>
2020-04-03 03:41:12 +0530

    Add login logic

--- Pages/Login.cshtml.cs
+++ Pages/Login.cshtml.cs
@@ -1,20 +1,33 @@
+using System.Security.Cryptography.X509Certificates;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using MongoDB.Bson;
+using MongoDB.Driver;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using NCBank.Models;
 
-namespace NCBank.Pages
-{
-    public class LoginModel : PageModel
-    {
+namespace NCBank.Pages {
+    public class LoginModel : PageModel {
         [BindProperty]
         public LoggedInCustomer cust {get; set; }
 
-        public void OnGet()
-        {
+        public void OnGet() {
+        }
+
+        public async Task<IActionResult> OnPostAsync() {
+            var filter = Builders<BsonDocument>.Filter.Eq("email", cust.Email);
+
+            var projection = Builders<BsonDocument>.Projection.Include("email").Include("passwordHash");
+            // verify email is a valid account
+            var user = await DBInterface.cust.Find(filter).Project(projection).SingleOrDefaultAsync();
+            // verify password
+            if (cust.verifyPassword(user.GetValue("passwordHash").ToString())) {
+                return RedirectToPage("/Dashboard");
+            }
+            return Page();
         }
     }
 }
