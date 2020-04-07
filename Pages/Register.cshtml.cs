f61f3b3edd3bae67bea8ffde3a5dcc0b199e5145
Nikhil Hari <nikhil_hari@live.com>
2020-04-07 09:39:26 +0530

    Login after Registration

--- Pages/Register.cshtml.cs
+++ Pages/Register.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using MongoDB.Bson;
+using Microsoft.AspNetCore.Http;
 using MongoDB.Driver;
 using NCBank.Models;
 
@@ -11,6 +12,8 @@ namespace NCBank.Pages {
         public BankCustomer customer {get; set; }
         
         public void OnPost() {
+            var sess = SessionManager.InsertSession(customer);
+            sess.Start();
             DBInterface.cust.InsertOne(customer.ToBsonDocument());
 
             CustomerBalance balance = new CustomerBalance();
@@ -18,7 +21,11 @@ namespace NCBank.Pages {
             balance.Balance = 0;
 
             DBInterface.cust.InsertOne(balance.ToBsonDocument());
-            Response.Redirect("/Index");
+            
+            sess.Wait();
+            HttpContext.Session.SetString("SessionID", sess.Result.SessionID);
+
+            Response.Redirect("/Dashboard");
         }
     }
 }
