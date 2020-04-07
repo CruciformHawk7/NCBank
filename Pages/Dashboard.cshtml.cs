a8b3c1b898896fa0fd22a6f3d8706d9e11662e9e
Nikhil Hari <nikhil_hari@live.com>
2020-04-07 09:39:54 +0530

    Redirect to Index if Signed out

--- Pages/Dashboard.cshtml.cs
+++ Pages/Dashboard.cshtml.cs
@@ -1,9 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Http;
 
 namespace NCBank.Pages {
     public class Dashboard : PageModel {
+        [BindProperty]
+        public string User {get; set; }
         public void OnGet() {
-            
+            if (HttpContext.Session.GetString("sessionID")==null || HttpContext.Session.GetString("sessioID")!="") {
+                Response.Redirect("Index");
+            }
         }
     }
 }
