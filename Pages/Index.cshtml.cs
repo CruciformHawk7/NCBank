c8d4f675ae1f2d45d3f8ddc443041b4382e5b015
Nikhil Hari <nikhil_hari@live.com>
2020-04-09 05:51:16 +0530

    Add Login method in Index

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -5,21 +5,34 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using NCBank.Models;
+using Microsoft.AspNetCore.Http;
+using MongoDB.Driver;
 
-namespace NCBank.Pages
-{
-    public class IndexModel : PageModel
-    {
-        private readonly ILogger<IndexModel> _logger;
+namespace NCBank.Pages{
+    public class IndexModel : PageModel{
+        [BindProperty]
+        public LoggedInCustomer cust {get; set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
-        {
-            _logger = logger;
+        public IActionResult OnGet() {
+            if (!string.IsNullOrEmpty(HttpContext.Session.GetString("sessionID"))) {
+                return RedirectToPage("Dashboard");
+            } else {
+                return Page();
+            }
         }
 
-        public void OnGet()
-        {
-
+        public async Task<IActionResult> OnPostAsync() {
+            var filter = Builders<BankCustomer>.Filter.Eq("email", cust.Email);
+            var projection = Builders<BankCustomer>.Projection.Include("email").Include("passwordHash");
+            var doc = await DBInterface.cust.Find(filter).Project(projection).FirstOrDefaultAsync();
+            var user = BankCustomer.ToBankCustomer(doc);
+            if (cust.Password.Equals(user.Password)) {
+                var sess = await SessionManager.InsertSession(user);
+                HttpContext.Session.SetString("sessionID", (sess.SessionID));
+                return RedirectToPage("/Dashboard");
+            }
+            return Page();
         }
     }
 }
