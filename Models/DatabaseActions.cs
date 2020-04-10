63a0cd78924da0f4be9c9af4bc77df38b0c8d928
Nikhil Hari <nikhil_hari@live.com>
2020-04-10 10:47:51 +0530

    Transactions.

--- Models/DatabaseActions.cs
+++ Models/DatabaseActions.cs
@@ -2,11 +2,11 @@ using MongoDB.Bson;
 using MongoDB.Driver;
 
 namespace NCBank.Models {
-    static class DBInterface {
+    public static class DBInterface {
         static private IMongoDatabase db = new MongoClient().GetDatabase("bankNC");
         static internal IMongoCollection<BankCustomer> cust = db.GetCollection<BankCustomer>("customers");
         static internal IMongoCollection<Sessions> sess = db.GetCollection<Sessions>("sessions");
         static internal IMongoCollection<CustomerBalance> bal = db.GetCollection<CustomerBalance>("balance");
-        static internal IMongoCollection<Transaction> tran = db.GetCollection<Transaction>("transactions");
+        static public IMongoCollection<Transaction> tran = db.GetCollection<Transaction>("transactions");
     }
 }
