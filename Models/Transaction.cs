8fe436c5c46c3469c0818bb40b074188256d2d00
Nikhil Hari <nikhil_hari@live.com>
2020-04-13 20:04:22 +0530

    Complete transaction code

--- Models/Transaction.cs
+++ Models/Transaction.cs
@@ -1,5 +1,9 @@
+using System.Collections.Immutable;
+using System;
+using System.Threading.Tasks;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
 
 namespace NCBank.Models {
     public class Transaction {
@@ -7,8 +11,30 @@ namespace NCBank.Models {
         [BsonElement("from")] public string FromEmail {get; set; }
         [BsonElement("to")] public string ToEmail {get; set; }
         [BsonElement("amount")] public double Amount {get; set; }
+        [BsonElement("time")] public DateTime Time {get; set;}
         [BsonElement("comment")] public string Comment {get; set; }
         [BsonElement("type")] public TransactionType Type {get; set; }
+
+        public static async Task DoTransaction(string from, string to, double amount, string comment, 
+                TransactionType type) {
+            Transaction tr = new Transaction() {
+                FromEmail = from,
+                ToEmail = to,
+                Amount = amount,
+                Comment = comment,
+                Type = type,
+                Time = DateTime.Now
+            };
+            await DBInterface.tran.InsertOneAsync(tr);
+            
+            var _FromFilter = Builders<CustomerBalance>.Filter.Eq("email", from);
+            var _FromUpd = Builders<CustomerBalance>.Update.Inc("balance", -1*amount);
+            await DBInterface.bal.FindOneAndUpdateAsync(_FromFilter, _FromUpd);
+
+            var _ToFilter = Builders<CustomerBalance>.Filter.Eq("email", to);
+            var _ToUpd = Builders<CustomerBalance>.Update.Inc("balance", amount);
+            await DBInterface.bal.FindOneAndUpdateAsync(_ToFilter, _ToUpd);
+        }
     }
 
     public enum TransactionType {
