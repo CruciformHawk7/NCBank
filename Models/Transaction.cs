78603a1b47c797e82febb7a44be9c456ba7fc5f0
Nikhil Hari <nikhil_hari@live.com>
2020-04-14 18:53:51 +0530

    Allow Payments

--- Models/Transaction.cs
+++ Models/Transaction.cs
@@ -1,17 +1,30 @@
+using System.Linq;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Collections.Immutable;
 using System;
 using System.Threading.Tasks;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations;
 using MongoDB.Driver;
 
 namespace NCBank.Models {
     public class Transaction {
         [BsonId] public ObjectId Id {get; set; }
         [BsonElement("from")] public string FromEmail {get; set; }
-        [BsonElement("to")] public string ToEmail {get; set; }
-        [BsonElement("amount")] public double Amount {get; set; }
-        [BsonElement("time")] public DateTime Time {get; set;}
+
+        [BsonElement("to")] 
+        [Required(ErrorMessage="Specify your sender here.")]
+        [DataType(DataType.EmailAddress, ErrorMessage="Invalid Email Address")]
+        public string ToEmail {get; set; }
+
+
+        [BsonElement("amount")] 
+        [Required(ErrorMessage="Enter the amount to send")]
+        [DataType(DataType.Currency)]
+        public double Amount {get; set; }
+        [BsonElement("time")] public DateTime Time {get; private set;}
         [BsonElement("comment")] public string Comment {get; set; }
         [BsonElement("type")] public TransactionType Type {get; set; }
 
@@ -34,6 +47,25 @@ namespace NCBank.Models {
             var _ToFilter = Builders<CustomerBalance>.Filter.Eq("email", to);
             var _ToUpd = Builders<CustomerBalance>.Update.Inc("balance", amount);
             await DBInterface.bal.FindOneAndUpdateAsync(_ToFilter, _ToUpd);
+        }
+
+        public static async Task DoTransaction(Transaction transaction) {
+            await DBInterface.tran.InsertOneAsync(transaction);
+            
+            var _FromFilter = Builders<CustomerBalance>.Filter.Eq("email", transaction.FromEmail);
+            var _FromUpd = Builders<CustomerBalance>.Update.Inc("balance", -1*transaction.Amount);
+            await DBInterface.bal.FindOneAndUpdateAsync(_FromFilter, _FromUpd);
+
+            var _ToFilter = Builders<CustomerBalance>.Filter.Eq("email", transaction.ToEmail);
+            var _ToUpd = Builders<CustomerBalance>.Update.Inc("balance", transaction.Amount);
+            await DBInterface.bal.FindOneAndUpdateAsync(_ToFilter, _ToUpd);
+        }
+
+        public static bool CheckEmail(string email) {
+            var Filter = Builders<BankCustomer>.Filter.Eq("email", email);
+            var list = DBInterface.cust.Find(Filter).ToList();
+            if (list.Count == 0) return false;
+            else return true;
         }
     }
 
--- Pages/Payments.cshtml.cs
+++ Pages/Payments.cshtml.cs
@@ -1,16 +1,36 @@
+using System.Transactions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using NCBank.Models;
+using Microsoft.AspNetCore.Http;
 
-namespace NCBank.Pages
-{
-    public class PaymentsModel : PageModel
-    {
-        public void OnGet()
-        {
+namespace NCBank.Pages {
+    public class PaymentsModel : PageModel {
+        [BindProperty] public Models.Transaction transaction {get; set;}
+        [ViewData] public string ToEmailError {get; set; }
+        public void OnGet() {
+
+        }
+
+        public async Task<IActionResult> OnPostAsync() {
+            if (ModelState.IsValid) {
+                if (Models.Transaction.CheckEmail(transaction.ToEmail)) {
+                    BankCustomer user = SessionManager.GetSession(HttpContext.Session.GetString("sessionID"));
+                    transaction.FromEmail = user.Email;
+                    transaction.Type = TransactionType.NetBanking;
+                    await Models.Transaction.DoTransaction(transaction);
+                    return Page();
+                } else {
+                    ToEmailError = "This user does not exist.";
+                    return Page();
+                } 
+            } else {
+                return Page();
+            }
         }
     }
 }
