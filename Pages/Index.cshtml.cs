541acf6a0885d0b6217c6d45da89c57d3780ec9b
Nikhil Hari <nikhil_hari@live.com>
2020-04-15 23:43:22 +0530

    Annotate login

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 using Microsoft.Extensions.Logging;
 using NCBank.Models;
 using Microsoft.AspNetCore.Http;
@@ -14,7 +15,10 @@ namespace NCBank.Pages{
         [BindProperty]
         public LoggedInCustomer cust {get; set; }
 
+        [ViewData] public string LoginErr {get; set;}
+
         public IActionResult OnGet() {
+            LoginErr = "";
             if (!string.IsNullOrEmpty(HttpContext.Session.GetString("sessionID"))) {
                 return RedirectToPage("Dashboard");
             } else {
@@ -26,11 +30,17 @@ namespace NCBank.Pages{
             var filter = Builders<BankCustomer>.Filter.Eq("email", cust.Email);
             var projection = Builders<BankCustomer>.Projection.Include("email").Include("passwordHash");
             var doc = await DBInterface.cust.Find(filter).Project(projection).FirstOrDefaultAsync();
+            if (doc==null) {
+                LoginErr = "You don't have an account. Sign up?";
+                return Page();
+            }
             var user = BankCustomer.ToBankCustomer(doc);
             if (cust.Password.Equals(user.Password)) {
                 var sess = await SessionManager.InsertSession(user);
                 HttpContext.Session.SetString("sessionID", (sess.SessionID));
                 return RedirectToPage("/Dashboard");
+            } else {
+                LoginErr = "Invalid username or password. Please try again.";
             }
             return Page();
         }
